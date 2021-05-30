006cc58621e3ea510f5f673020cfec45c3379813
Felipe <felipejm9805@gmail.com>
2021-05-30 17:50:14 -0500

    add update code

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('index.html').pipe(res)
+
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, { 'content-type': 'text/html' })
+      fs.createReadStream('index.html').pipe(res)
+      break
+    case "/enviar":
+      res.writeHead(200);
+      res.end(JSON.stringify("Enviado"));
+
+      break
+    default:
+        res.writeHead(404);
+        res.end(JSON.stringify({error:"Resource not found"}));
+}
+
 })
+
+
 
 server.listen(3000, () => {
   console.log(`Example app listening at http://localhost:${3000}`)
